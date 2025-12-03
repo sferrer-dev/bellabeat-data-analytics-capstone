0164538728c855ce6e2e82f60e1ed1e32fe0c19d
Stephane <3736753+SFERRER-DEV@users.noreply.github.com>
2025-12-03 18:40:30 +0100

    SSDT : Ajouter la vue SQL Server rpt.vDailyActivity_Insights

---
+++ src/ETL.Bellabeat.Database/Views/rpt/vDailyActivity_Insights.sql
@@ -0,0 +1,30 @@
+﻿CREATE VIEW [rpt].[vDailyActivity_Insights]
+	AS
+
+SELECT
+    Id,
+    ActivityDate,
+-- Activité — mesures principales
+    TotalSteps,
+    TotalDistance,
+    VeryActiveMinutes,
+    FairlyActiveMinutes,
+    LightlyActiveMinutes,
+    SedentaryMinutes,
+-- Activité combinée (utile pour des graphiques)
+    VeryActiveMinutes 
+        + FairlyActiveMinutes 
+        + LightlyActiveMinutes AS TotalActiveMinutes,
+-- Calories
+    Calories,
+-- Jour de la semaine
+    DATENAME(WEEKDAY, ActivityDate) AS DayOfWeek,
+    DATEPART(WEEKDAY, ActivityDate) AS DayOfWeekNumber,
+-- Semaine vs week-end
+    CASE 
+        -- Weekday / Weekend (robuste)
+        WHEN FORMAT(ActivityDate, 'ddd', 'en-US') IN ('Sat', 'Sun')
+        THEN 'Weekend'
+        ELSE 'Weekday'
+    END AS PartOfWeek
+FROM dbo.DailyActivity;
