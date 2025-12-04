e51d30751452d91e151344358e429ec23ad9cf0e
Stephane <3736753+SFERRER-DEV@users.noreply.github.com>
2025-12-04 11:44:33 +0100

    SSDT :  Ajouter la vue pour l'analyse fréquence cardiaque

---
+++ src/ETL.Bellabeat.Database/Views/rpt/vHeartrate_DailyBiologicalRhythm.sql
@@ -0,0 +1,28 @@
+﻿CREATE VIEW [rpt].[vHeartrate_DailyBiologicalRhythm] 
+AS
+
+SELECT
+    hs.Id,
+    CAST(hs.[Time] AS date) AS ActivityDate,
+        
+    CASE
+        WHEN DATEPART(HOUR, hs.[Time]) >= 6  AND DATEPART(HOUR, hs.[Time]) < 12 THEN 'Morning'
+        WHEN DATEPART(HOUR, hs.[Time]) >= 12 AND DATEPART(HOUR, hs.[Time]) < 18 THEN 'Afternoon'
+        WHEN DATEPART(HOUR, hs.[Time]) >= 18 AND DATEPART(HOUR, hs.[Time]) < 21 THEN 'Evening'
+        ELSE 'Night'
+    END AS TimeOfDay,
+-- Agrégations de fréquence cardiaque
+    AVG(CAST(hs.[Value] AS decimal(10,2))) AS AvgHeartRate,
+    MIN(hs.[Value])                        AS MinHeartRate,
+    MAX(hs.[Value])                        AS MaxHeartRate,
+    COUNT(*)                               AS NbSeconds
+FROM dbo.HeartrateSeconds AS hs
+GROUP BY
+    hs.Id,
+    CAST(hs.[Time] AS date),
+    CASE
+        WHEN DATEPART(HOUR, hs.[Time]) >= 6  AND DATEPART(HOUR, hs.[Time]) < 12 THEN 'Morning'
+        WHEN DATEPART(HOUR, hs.[Time]) >= 12 AND DATEPART(HOUR, hs.[Time]) < 18 THEN 'Afternoon'
+        WHEN DATEPART(HOUR, hs.[Time]) >= 18 AND DATEPART(HOUR, hs.[Time]) < 21 THEN 'Evening'
+        ELSE 'Night'
+    END;
