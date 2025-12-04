707f902c6dca25b933023bb3e0822feaa0afd2ca
Stephane <3736753+SFERRER-DEV@users.noreply.github.com>
2025-12-04 14:55:22 +0100

    SSDT: Modification de la vue vHeartrate_DailyBiologicalRhythm dans le but de simplfier le Data Frame

--- src/ETL.Bellabeat.Database/Views/rpt/vHeartrate_DailyBiologicalRhythm.sql
+++ src/ETL.Bellabeat.Database/Views/rpt/vHeartrate_DailyBiologicalRhythm.sql
@@ -1,28 +1,37 @@
 ﻿CREATE VIEW [rpt].[vHeartrate_DailyBiologicalRhythm] 
 AS
+    SELECT
+        hs.Id,
+        CAST(hs.[Time] AS date) AS ActivityDate,
 
-SELECT
-    hs.Id,
-    CAST(hs.[Time] AS date) AS ActivityDate,
-        
-    CASE
-        WHEN DATEPART(HOUR, hs.[Time]) >= 6  AND DATEPART(HOUR, hs.[Time]) < 12 THEN 'Morning'
-        WHEN DATEPART(HOUR, hs.[Time]) >= 12 AND DATEPART(HOUR, hs.[Time]) < 18 THEN 'Afternoon'
-        WHEN DATEPART(HOUR, hs.[Time]) >= 18 AND DATEPART(HOUR, hs.[Time]) < 21 THEN 'Evening'
-        ELSE 'Night'
-    END AS TimeOfDay,
--- Agrégations de fréquence cardiaque
-    AVG(CAST(hs.[Value] AS decimal(10,2))) AS AvgHeartRate,
-    MIN(hs.[Value])                        AS MinHeartRate,
-    MAX(hs.[Value])                        AS MaxHeartRate,
-    COUNT(*)                               AS NbSeconds
-FROM dbo.HeartrateSeconds AS hs
-GROUP BY
-    hs.Id,
-    CAST(hs.[Time] AS date),
-    CASE
-        WHEN DATEPART(HOUR, hs.[Time]) >= 6  AND DATEPART(HOUR, hs.[Time]) < 12 THEN 'Morning'
-        WHEN DATEPART(HOUR, hs.[Time]) >= 12 AND DATEPART(HOUR, hs.[Time]) < 18 THEN 'Afternoon'
-        WHEN DATEPART(HOUR, hs.[Time]) >= 18 AND DATEPART(HOUR, hs.[Time]) < 21 THEN 'Evening'
-        ELSE 'Night'
-    END;
+-- Time of day classification
+        CASE
+            WHEN DATEPART(HOUR, hs.[Time]) >= 6  AND DATEPART(HOUR, hs.[Time]) < 12 THEN 'Morning'
+            WHEN DATEPART(HOUR, hs.[Time]) >= 12 AND DATEPART(HOUR, hs.[Time]) < 18 THEN 'Afternoon'
+            WHEN DATEPART(HOUR, hs.[Time]) >= 18 AND DATEPART(HOUR, hs.[Time]) < 21 THEN 'Evening'
+            ELSE 'Night'
+        END AS TimeOfDay,
+
+-- Day of week number (1 = Monday, ..., 7 = Sunday)
+        ((DATEPART(WEEKDAY, hs.[Time]) + @@DATEFIRST - 2) % 7) + 1 AS DayOfWeekNumber,
+
+-- Day of week name (depends on server language)
+        DATENAME(WEEKDAY, hs.[Time]) AS DayOfWeekName,
+
+-- Heart rate aggregations
+        AVG(CAST(hs.[Value] AS decimal(10,2))) AS AvgHeartRate,
+        MIN(hs.[Value])                        AS MinHeartRate,
+        MAX(hs.[Value])                        AS MaxHeartRate,
+        COUNT(*)                               AS NbSeconds
+    FROM dbo.HeartrateSeconds AS hs
+    GROUP BY
+        hs.Id,
+        CAST(hs.[Time] AS date),
+        CASE
+            WHEN DATEPART(HOUR, hs.[Time]) >= 6  AND DATEPART(HOUR, hs.[Time]) < 12 THEN 'Morning'
+            WHEN DATEPART(HOUR, hs.[Time]) >= 12 AND DATEPART(HOUR, hs.[Time]) < 18 THEN 'Afternoon'
+            WHEN DATEPART(HOUR, hs.[Time]) >= 18 AND DATEPART(HOUR, hs.[Time]) < 21 THEN 'Evening'
+            ELSE 'Night'
+        END,
+        ((DATEPART(WEEKDAY, hs.[Time]) + @@DATEFIRST - 2) % 7) + 1,
+        DATENAME(WEEKDAY, hs.[Time]);
